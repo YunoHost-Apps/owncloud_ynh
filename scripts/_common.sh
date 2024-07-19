3ff695382ee1de246e8108320638d5b29488ba94
Éric Gaspar <46165813+ericgaspar@users.noreply.github.com>
2024-07-19 10:16:22 +0200

    cleaning

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -3,3 +3,5 @@
 #=================================================
 # COMMON VARIABLES AND CUSTOM HELPERS AND CUSTOM HELPERS
 #=================================================
+
+timezone=$(cat /etc/timezone)
